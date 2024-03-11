1e1f313da39fb787b70285534576f5ba77377e80
RASantos92 <rasbms81608@gmail.com>
2024-03-11 12:27:21 -0500

    had to do reorderList in-place

--- 75InterviewProblems/LinkedList/Python/ReorderList.py
+++ 75InterviewProblems/LinkedList/Python/ReorderList.py
@@ -7,6 +7,7 @@ class ListNode:
         self.next = next
 
 class Solution:
+    #This is not in place
     def reorderList(self, head: Optional[ListNode]) -> None:
         listToArray = []
         runner = head.next
@@ -27,5 +28,29 @@ class Solution:
                 start += 1
                 switch = False
             runner = runner.next
+
+
+    def reorderList2(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second 
+            second = tmp
+        
+        first,second = head, prev
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
 
 
