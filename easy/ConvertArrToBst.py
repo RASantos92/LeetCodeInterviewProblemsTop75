b29568931313a8e03ae7f46da6e79f91236c2e28
RASantos92 <rasbms81608@gmail.com>
2024-03-20 11:38:46 -0500

    medium InsertDeleteGetRandomO(1)

---
+++ easy/ConvertArrToBst.py
@@ -0,0 +1,8 @@
+class TreeNode:
+    def __init__(self, val = None, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(nums)
---
+++ medium/380InsertDeleteGetRandomo1.py
@@ -0,0 +1,31 @@
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.set = set()
+
+    def insert(self, val: int) -> bool:
+        if val in self.set:
+            return False
+        self.set.add(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val in self.set:
+            self.set.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(list(self.set))
+    
+test = RandomizedSet()
+
+test.insert(1)
+test.insert(2)
+test.insert(3)
+test.insert(4)
+print(test.set)
+print(test.getRandom(),test.getRandom(),test.getRandom(),test.getRandom(),test.getRandom(), test.getRandom(),test.getRandom(),test.getRandom())
